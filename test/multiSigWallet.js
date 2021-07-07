c36b2a31c16b32da694f0ef46b28f6409ab9de1b
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-07-07 12:20:43 +0200

    Only approver can call

--- test/multiSigWallet.js
+++ test/multiSigWallet.js
@@ -1,26 +1,31 @@
-const { expectRevert } = require("@openzeppelin/test-helpers")
+const { expectRevert } = require('@openzeppelin/test-helpers');
 
-const MultiSigWallet = artifacts.require("MultiSigWallet");
+const MultiSigWallet = artifacts.require('MultiSigWallet');
 
-contract("MultiSigWallet", (accounts) => {
-  let [approver1, approver2, approver3] = accounts
-  let multiSigWallet
+contract('MultiSigWallet', (accounts) => {
+  let [approver1, approver2, approver3, nonApprover] = accounts;
+  let multiSigWallet;
 
   before(async () => {
-    multiSigWallet = await MultiSigWallet.deployed()
-  })
+    multiSigWallet = await MultiSigWallet.deployed();
+  });
 
-  describe("createTransfer", () => {
-    it("Creates a transfer", async () => {
-      await multiSigWallet.createTransfer(1000, accounts[4], { from: approver1 })
-      const transfer = await multiSigWallet.transfers(0)
+  describe('createTransfer', () => {
+    it('Creates a transfer', async () => {
+      await multiSigWallet.createTransfer(1000, accounts[4], { from: approver1 });
+      const transfer = await multiSigWallet.transfers(0);
 
-      assert.equal(transfer.id.toNumber(), 0)
-      assert.equal(transfer.amount.toNumber(), 1000)
-    })
-  })
+      assert.equal(transfer.id.toNumber(), 0);
+      assert.equal(transfer.amount.toNumber(), 1000);
+    });
 
-  describe("sendTransfer", () => {
-    
-  })
+    it('Fails when called by non-approver account', async () => {
+      await expectRevert(
+        multiSigWallet.createTransfer(1000, accounts[4], { from: nonApprover }),
+        'Only approver allowed'
+      );
+    });
+  });
+
+  describe('sendTransfer', () => {});
 });
