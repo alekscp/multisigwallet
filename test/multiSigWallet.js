be59f77d9bd38f371e1fbdd1714fe373e6760b74
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-07-07 15:24:50 +0200

    Finish tests

--- test/multiSigWallet.js
+++ test/multiSigWallet.js
@@ -6,10 +6,10 @@ contract('MultiSigWallet', (accounts) => {
   let [approver1, approver2, approver3, recipient1, recipient2, nonApprover] = accounts;
   let multiSigWallet;
 
-  let transferId = 0
+  let transferId = 0;
 
   before(async () => {
-    multiSigWallet = await MultiSigWallet.deployed()
+    multiSigWallet = await MultiSigWallet.deployed();
   });
 
   describe('createTransfer', () => {
@@ -20,32 +20,57 @@ contract('MultiSigWallet', (accounts) => {
       assert.equal(transfer.id.toNumber(), transferId);
       assert.equal(transfer.amount.toNumber(), 1000);
 
-      transferId++
+      transferId++;
     });
 
     it('Fails when called by non-approver account', async () => {
       await expectRevert(
-        multiSigWallet.createTransfer(1000, nonApprover, { from: nonApprover }),
+        multiSigWallet.createTransfer(1000, recipient1, { from: nonApprover }),
         'Only approver allowed'
       );
     });
   });
 
   describe('sendTransfer', () => {
-    it('Sends transfer if quorum is reached', async () => {
+    it('Does not transfer if quorum is not reached', async () => {
       const recipientBalanceBefore = web3.utils.toBN(await web3.eth.getBalance(recipient2));
 
-      await multiSigWallet.createTransfer(1000, recipient2, { from: approver1 })
-      await multiSigWallet.sendTransfer(transferId, { from: approver2 })
-      await multiSigWallet.sendTransfer(transferId, { from: approver3 })
+      await multiSigWallet.createTransfer(1000, recipient2, { from: approver1 });
+      await multiSigWallet.sendTransfer(transferId, { from: approver2 });
 
       const recipientBalanceAfter = web3.utils.toBN(await web3.eth.getBalance(recipient2));
 
-      const difference = recipientBalanceAfter.sub(recipientBalanceBefore)
+      const difference = recipientBalanceAfter.sub(recipientBalanceBefore);
 
-      assert.equal(difference.toNumber(), 1000)
+      assert.equal(difference.toNumber(), 0);
 
-      transferId++
-    })
+      transferId++;
+    });
+
+    it('Sends transfer if quorum is reached', async () => {
+      const recipientBalanceBefore = web3.utils.toBN(await web3.eth.getBalance(recipient2));
+
+      await multiSigWallet.createTransfer(1000, recipient2, { from: approver1 });
+      await multiSigWallet.sendTransfer(transferId, { from: approver2 });
+      await multiSigWallet.sendTransfer(transferId, { from: approver3 });
+
+      const recipientBalanceAfter = web3.utils.toBN(await web3.eth.getBalance(recipient2));
+
+      const difference = recipientBalanceAfter.sub(recipientBalanceBefore);
+
+      assert.equal(difference.toNumber(), 1000);
+
+      transferId++;
+    });
+
+    it('Fails when called by non-approver account', async () => {
+      await multiSigWallet.createTransfer(1000, recipient2, { from: approver1 });
+      transferId++;
+
+      await expectRevert(
+        multiSigWallet.sendTransfer(transferId, { from: nonApprover }),
+        'Only approver allowed'
+      );
+    });
   });
 });
