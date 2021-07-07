5cf9db8ea0ac6aaf4b36c9534dd9ddba6690b7d2
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-07-07 09:52:49 +0200

    Setup data structures

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -1,11 +1,27 @@
 pragma solidity ^0.8.4;
 
 contract MultiSigWallet {
-  address[] public approvers;
-  uint public quorum;
+    address[] public approvers;
+    uint public quorum;
 
-  constructor(address[] memory _approvers, uint _quorum) public payable {
-    approvers = _approvers
-    quorum = _quorum
-  }
+    struct Transfer {
+        uint id;
+        uint amount;
+        address payable to;
+        uint approvals;
+        bool sent;
+    }
+    mapping(uint => Transfer) transfers;
+
+    uint nextId;
+
+    constructor(address[] memory _approvers, uint _quorum) public payable {
+        approvers = _approvers;
+        quorum = _quorum;
+    }
+
+    function createTransfer(uint amount, address payable to) external {
+        // Container
+        // Transfer
+    }
 }
