2b749433bcbc52c7be0daab698a962bfa80282d3
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-07-08 12:01:18 +0200

    Add migration for goerli

--- migrations/1625648423_multi_sig_wallet.js
+++ migrations/1625648423_multi_sig_wallet.js
@@ -3,10 +3,17 @@ const MultiSigWallet = artifacts.require("MultiSigWallet")
 module.exports = function(deployer, network, accounts) {
   if (network == "development") {
     deployer.deploy(
-      MultiSigWallet,
+      multisigwallet,
       [accounts[0], accounts[1], accounts[2]],
       2,
-      { value: web3.utils.toWei("1", "ether") }
+      { value: web3.utils.towei("1", "ether") }
+    );
+  } else if (network == "goerli") {
+    deployer.deploy(
+      multisigwallet,
+      [accounts[0], accounts[1], accounts[2]],
+      2,
+      { value: web3.utils.towei("5", "ether") }
     );
   }
 };
