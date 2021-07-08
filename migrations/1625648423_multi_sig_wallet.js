1d976f38c921076a6573d271978ab08363d5282b
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-07-08 12:10:35 +0200

    Typo

--- migrations/1625648423_multi_sig_wallet.js
+++ migrations/1625648423_multi_sig_wallet.js
@@ -3,14 +3,14 @@ const MultiSigWallet = artifacts.require("MultiSigWallet")
 module.exports = function(deployer, network, accounts) {
   if (network == "development") {
     deployer.deploy(
-      multisigwallet,
+      Multisigwallet,
       [accounts[0], accounts[1], accounts[2]],
       2,
       { value: web3.utils.towei("1", "ether") }
     );
   } else if (network == "goerli") {
     deployer.deploy(
-      multisigwallet,
+      Multisigwallet,
       [accounts[0], accounts[1], accounts[2]],
       2,
       { value: web3.utils.towei("5", "ether") }
